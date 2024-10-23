36a832096ac95903a2615ae900f49245075887b6
Vlad KeyKey <vladkeykey@Vlads-MacBook-Air.local>
2024-10-23 09:28:19 +0300

    commit

--- bot.js
+++ bot.js
@@ -51,7 +51,6 @@ bot.onText(/\/start/, async (msg) => {
 
 bot.onText(/Головне меню/, async (msg) => {
     const chatId = msg.chat.id;
-  
     await bot.sendMessage(chatId, 'Обирайте кнопками, що цікавить👇', mainMenu);
 });
 
